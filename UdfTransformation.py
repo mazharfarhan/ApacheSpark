d3511a623c85f00a5ffe76330c1dd198db3aa458
Farhan Mazhar <fmazhar@usc.edu>
2016-09-10 23:18:27 -0700

    Update UdfTransformation.py

--- UdfTransformation.py
+++ UdfTransformation.py
@@ -4,3 +4,6 @@ df = sqlContext.createDataFrame(data,['name', 'age'])
 # create a user defined transformation that returns the double of integer value.
 doubled = udf(lamda s: s*2, Integer())
 df2 = df.select(df.name, doubled(df.age).alias('age'))
+
+#Transformation to get age that is greater than 3
+df3 = df2.filter(df2.age > 3)
